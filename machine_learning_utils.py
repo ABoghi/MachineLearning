59a2591e763fe2ed8bd0595b4b6dda4274e7f639
Andrea Boghi <Andrea.BOGHI@nats.co.uk>
2019-06-06 15:00:18 +0100

    Second Commit.

--- machine_learning_utils.py
+++ machine_learning_utils.py
@@ -2,6 +2,10 @@ import numpy as np
 
 import random as rm
 
+import matplotlib
+
+from matplotlib import pyplot as plt
+
 def sigmoid(z) :
     '''
     Inputs
@@ -42,7 +46,7 @@ def verifyLen(y,h) :
     if n != m :
         quit()
 
-    return
+    return n
 
 def LinearCostFunction(y,h) :
     '''
@@ -56,7 +60,7 @@ def LinearCostFunction(y,h) :
     J []: (float)
     '''
 
-    verifyLen(y,h)
+    n = verifyLen(y,h)
 
     J = 0.0
     for i in range(n) :
@@ -77,7 +81,7 @@ def LogisticCostFunction(y,h) :
     J []: (float)
     '''
 
-    verifyLen(y,h)
+    n = verifyLen(y,h)
 
     J = 0.0
     for i in range(n) :
@@ -87,12 +91,115 @@ def LogisticCostFunction(y,h) :
 
 def PolynomialModel(x,theta) :
     
+    '''
+    Inputs
+    ------
+    x []: (float) 
+    theta []: (float)
+
+    Outputs
+    -------
+    h []: (float)
+    '''
     n = len(x)
     m = len(theta)
 
+    print(theta)
+    print(m)
+
     h = np.zeros(n)
     for j in range(m) :
+        print("j = "+str(j))
         for i in range(n) :
-            h[i] = theta[j] * pow(x[i],j)
+            h[i] = h[i] + theta[j] * pow(x[i],j)
 
     return h
+
+def SigmoidalModel(x,theta) :
+    
+    '''
+    Inputs
+    ------
+    x []: (float) 
+    theta []: (float)
+
+    Outputs
+    -------
+    h []: (float)
+    '''
+    n = len(x)
+    m = len(theta)
+
+    print(theta)
+    print(m)
+
+    z = np.zeros(n)
+    for j in range(m) :
+        print("j = "+str(j))
+        for i in range(n) :
+            z[i] = z[i] + theta[j] * pow(x[i],j)
+
+    h = np.zeros(n)
+    for i in range(n) :
+        h[i] = sigmoid(z[i])
+
+    return h
+
+def LinearCostGradient(x,y,h,theta) :
+    '''
+    Inputs
+    ------
+    x []: (float)
+    y []: (float) 
+    h []: (float)
+    theta []: (float)
+
+    Outputs
+    -------
+    J []: (float)
+    '''
+
+    n = verifyLen(y,h)
+    m = len(theta)
+
+    
+    gradh = np.zeros(m)
+    for j in range(m) :
+        for i in range(n) :
+            gradh[j] = gradh[j] + pow(x[i],j)
+
+    gradJ = np.zeros(m)
+    for j in range(m) :
+        for i in range(n) :
+            gradJ[j] = gradJ[j] + ((h[i]-y[i])/n) * gradh[j]
+
+    return gradJ
+
+def LogisticCostGradient(x,y,h,theta) :
+    '''
+    Inputs
+    ------
+    x []: (float)
+    y []: (float) 
+    h []: (float)
+    theta []: (float)
+
+    Outputs
+    -------
+    J []: (float)
+    '''
+
+    n = verifyLen(y,h)
+    m = len(theta)
+
+    gradz = np.zeros(m)
+    for j in range(m) :
+        for i in range(n) :
+            gradz[j] = gradz[j] + pow(x[i],j)
+
+    gradJ = np.zeros(m)
+    for j in range(m) :
+        for i in range(n) :
+            gradJ[j] = gradJ[j] + ( h[i] - y[i] ) / n * gradz[j]
+
+    return gradJ
